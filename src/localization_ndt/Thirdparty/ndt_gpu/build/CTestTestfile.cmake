648469199545eb3a6822b500c2e0733e08653f3d
Eagleflag88 <yijiang.xie@foxmail.com>
2021-03-18 20:28:46 +0800

    remove ndt_gpu/build

--- src/localization_ndt/Thirdparty/ndt_gpu/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/tools/MultiModalEvnPercption/src/localization_ndt/Thirdparty/ndt_gpu
-# Build directory: /work/tools/MultiModalEvnPercption/src/localization_ndt/Thirdparty/ndt_gpu/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
