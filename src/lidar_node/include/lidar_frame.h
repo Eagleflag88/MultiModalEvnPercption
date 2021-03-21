d0959a6ed63f42e148356d1558595b43548721e5
Eagleflag88 <yijiang.xie@foxmail.com>
2021-03-21 17:56:31 +0800

    Add lidar_node

---
+++ src/lidar_node/include/lidar_frame.h
@@ -0,0 +1,48 @@
+//
+// Created by eagleflag on 2021/3/21.
+//
+
+#ifndef MULTIMODALEVNPERCPTION_LIDAR_FRAME_H
+#define MULTIMODALEVNPERCPTION_LIDAR_FRAME_H
+
+#include "ros/ros.h"
+
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/approximate_voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/registration/ndt.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+class lidar_frame
+{
+public:
+    typedef std::shared_ptr<lidar_frame> ptr;
+    unsigned long id_;
+    double time_stamp_;
+    Eigen::Isometry3d T_cw_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr scan_;
+
+public:
+    lidar_frame();
+    lidar_frame(
+            unsigned long id,
+            double time_stamp = 0.0,
+            Eigen::Isometry3d T_cw = Eigen::Isometry3d::Identity(),
+            pcl::PointCloud<pcl::PointXYZ>::Ptr scan = nullptr
+    );
+    ~lidar_frame();
+    ptr create_frame();
+    void filter();
+    void pub_odom(ros::Publisher odom_pub);
+
+};
+
+#endif //MULTIMODALEVNPERCPTION_LIDAR_FRAME_H
---
+++ src/lidar_node/src/lidar_node.cpp
@@ -0,0 +1,78 @@
+//
+// Created by eagleflag on 2021/3/21.
+//
+
+#include <iostream>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/NavSatFix.h>
+
+
+#include "lidar_frame.h"
+
+// Declaration of Publishers
+
+static ros::Publisher lidar_scan_pub;
+static ros::Publisher chatter_pub;
+
+// Declaration of Subscribers
+static ros::Subscriber lidar_scan_sub;
+
+//todo: 查看所有容器的使用，复制还是引用
+//todo: lock for data race
+
+static void PointCloud_Callback(const sensor_msgs::PointCloud2 curr_scan_msg)
+{
+    ROS_INFO("PointCloudCallback Started");
+    //Get current scan
+    pcl::PointCloud<pcl::PointXYZ>::Ptr scan_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(curr_scan_msg, *scan_cloud_ptr); //Convert point cloud msg to point cloud pointer for incoming laser scan
+    std::cout << "original number of point cloud is " << scan_cloud_ptr->width << std::endl;
+
+    lidar_frame::ptr lidar_frame_ptr = lidar_frame_ptr->create_frame();
+    lidar_frame_ptr->scan_ = scan_cloud_ptr;
+    lidar_frame_ptr->filter();
+
+    std::cout << "id of the cloud point frame ptr is " << lidar_frame_ptr->id_ << std::endl;
+
+    return;
+
+}
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "gnss_node");
+    ros::NodeHandle nh;
+    // Register the Subscriber
+    // todo:Add a parameter loading class
+    // %Tag(SUBSCRIBER)%
+    lidar_scan_sub = nh.subscribe("/kitti/velo/pointcloud", 10, PointCloud_Callback);
+
+    // %EndTag(SUBSCRIBER)%
+
+    // %Tag(PUBLISHER)%
+    chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+    lidar_scan_pub = nh.advertise<sensor_msgs::PointCloud2>("/lidar_scan", 10);
+
+    int count = 0;
+
+    std_msgs::String msg;
+    while(ros::ok())
+    {
+        std::stringstream status_msg;
+        status_msg << "lidar_node working fine " << count;
+        msg.data = status_msg.str();
+        ROS_INFO("%s", msg.data.c_str());
+        chatter_pub.publish(msg);
+        ros::spin();
+        count++;
+    }
+
+    return 0;
+}
