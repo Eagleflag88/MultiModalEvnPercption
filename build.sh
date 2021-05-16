c69106104f7fb0c411171d946f84643f3ef5864b
Eagleflag88 <yijiang.xie@foxmail.com>
2021-05-16 22:32:45 +0800

    relocate thirdparty folder

--- build.sh
+++ build.sh
@@ -2,19 +2,26 @@
 
 rm -rf build/ devel/
 
-cd src/localization_ndt/Thirdparty/g2o
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
-cd ../../../../..
+#cd src/localization_ndt/Thirdparty/g2o
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j8
+#cd ../../../../..
 
-cd src/localization_ndt/Thirdparty/GeographicLib
+cd src/thirdparty/g2o
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
-cd ../../../../..
+cd ../../../..
+
+#cd src/localization_ndt/Thirdparty/GeographicLib
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j8
+#cd ../../../../..
 
 cd src/localization_ndt/Thirdparty/ndt_gpu
 mkdir build
@@ -23,23 +30,34 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
 cd ../../../../..
 
-cd src/localization_ndt/Thirdparty/darknet
+#cd src/localization_ndt/Thirdparty/darknet
+#make
+#cd ../../../..
+
+cd src/thirdparty/darknet
 make
+cd ../../..
+
+cd src/thirdparty/GeographicLib
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j8
 cd ../../../..
 
-cd src/gnss_node/thirdparty/GeographicLib
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j8
-cd ../../../../..
+#cd src/localization_ndt/Thirdparty/DBoW2
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j8
+#cd ../../../../..
 
-cd src/localization_ndt/Thirdparty/DBoW2
+cd src/thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
-cd ../../../../..
+cd ../../../..
 
 catkin_make
 
